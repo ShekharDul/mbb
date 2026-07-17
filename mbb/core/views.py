6e7bd12b9bda8061d69281bd9c562fee579cb2d1
unknown <shekhardulgach19@gmail.com>
2026-07-17 11:37:24 +0530

    Fix core.views import issue

--- mbb/core/views.py
+++ mbb/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""Self Explaination: The logic here decides what happens when
+someone visits a URL"""
+
+def index(request):
+    return render(request, 'core/index.html')
+
